6aa9bd2be73745a0ab3b996a3930416989c9aff5
Nafisa Ebrahimi <nafisai2750@gmail.com>
2026-02-22 19:28:14 +0430

    upoad my project

---
+++ app.js
@@ -0,0 +1,53 @@
+// const navLinks = document.getElementById("nav-links");
+// const links = ["Home","About","Services","Contact"];
+// links.forEach((text) => {
+//   const li = document.createElement("li");
+//   const a = document.createElement("a");
+
+//   a.textContent = text;
+//   a.href = "#";
+
+//   li.appendChild(a);
+//   navLinks.appendChild(li);
+// });
+
+// for (let i = 0; i < 3; i++) {
+//   const line = document.createElement("div");
+//   line.className = "line";
+//   burger.appendChild(line);
+// }
+
+// navbar.appendChild(burger);
+
+// burger.addEventListener("click", () => {
+//   navLinks.classList.toggle("active");
+// });
+const navLinks = document.getElementById('nav-links');
+const navbar = document.querySelector('.navbar');
+const burger = document.createElement('div');
+burger.className = 'burger';
+
+const links = ['Home', 'About', 'Services', 'Contact'];
+
+links.forEach((text) => {
+  const li = document.createElement('li');
+  const a = document.createElement('a');
+
+  a.textContent = text;
+  a.href = '#';
+
+  li.appendChild(a);
+  navLinks.appendChild(li);
+});
+
+for (let i = 0; i < 3; i += 1) {
+  const line = document.createElement('div');
+  line.className = 'line';
+  burger.appendChild(line);
+}
+
+navbar.appendChild(burger);
+
+burger.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
